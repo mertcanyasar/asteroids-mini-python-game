71450ec15ef5cccf881958ef65a611f3a112f6be
mertcanyasar <canyasar924@gmail.com>
2025-09-17 14:17:45 +0300

    little changes

--- circleshape.py
+++ circleshape.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame # pyright: ignore[reportMissingImports]
 
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame # pyright: ignore[reportMissingImports]
 from constants import *
 
 def main():
