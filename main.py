1754d669cbc14590d44986469992d288298bc519
mertcanyasar <canyasar924@gmail.com>
2025-09-17 16:14:30 +0300

    Optimized game objects through adding sprite groups

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    FPS = 60
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
@@ -21,8 +23,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         rt = clock.tick(FPS)
         dt = rt/1000
