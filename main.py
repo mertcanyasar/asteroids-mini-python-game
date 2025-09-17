6f5fc9d628bb6fb984b9d9a1fa10b827ef38c66d
mertcanyasar <canyasar924@gmail.com>
2025-09-17 14:01:20 +0300

    clock tick addition

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main():
     pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -13,6 +14,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        rt = clock.tick(60)
+        dt = rt/1000
 
 
 if __name__ == "__main__":
